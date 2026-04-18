e29bd315eff31ec12279c4fd3afd927ff848686f
nkr <nithinkr2000@gmail.com>
2026-04-18 05:03:23 +0200

    Implemented Cholesky decomposition for strong type.

--- include/core/trajectory.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "base_types.hpp"
-
-struct Trajectory {
-    std::vector<Config> positions;
-    std::vector<Energy> energies;
-};
--- src/utils/helper_funcs.cpp
+++ src/utils/helper_funcs.cpp
@@ -1,26 +1,33 @@
+#ifndef MCMC_UTILS_CPP
+#define MCMC_UTILS_CPP
+
 #include "../../include/utils/helper_funcs.hpp"
 
-Matrix CholeskyDecomposition(const Matrix &matrix) {
+namespace mcmc::utils {
+void CholeskyDecomposition(mcmc::base_types::Matrix &matrix) {
 
-    size_t dim{matrix.size()};
-    Matrix lMatrix{dim, std::vector<double>(dim, 0.0f)};
+    size_t dim{matrix.M.size()};
+    matrix.L = std::vector<std::vector<double>>(dim, std::vector<double>(dim, 0.0f));
 
     for (size_t rIdx{0}; rIdx < dim; ++rIdx)
         for (size_t cIdx{0}; cIdx <= rIdx; ++cIdx) {
             if (cIdx == rIdx) {
                 double sumjk_1{0.0f};
                 for (size_t idx{0}; idx < cIdx - 1; ++idx)
-                    sumjk_1 += lMatrix[rIdx][idx] * lMatrix[rIdx][idx];
+                    sumjk_1 += matrix.L[rIdx][idx] * matrix.L[rIdx][idx];
 
-                lMatrix[rIdx][cIdx] = sqrt(matrix[rIdx][cIdx] - sumjk_1);
+                matrix.L[rIdx][cIdx] = sqrt(matrix.M[rIdx][cIdx] - sumjk_1);
             } else {
                 double sumjk_1{0.0f};
                 for (size_t idx{0}; idx < cIdx - 1; ++idx)
-                    sumjk_1 += lMatrix[cIdx][idx] * lMatrix[idx][rIdx];
+                    sumjk_1 += matrix.L[cIdx][idx] * matrix.L[idx][rIdx];
 
-                lMatrix[rIdx][cIdx] = sqrt(matrix[rIdx][cIdx] - sumjk_1) / lMatrix[rIdx][rIdx];
+                matrix.L[rIdx][cIdx] = sqrt(matrix.M[rIdx][cIdx] - sumjk_1) / matrix.L[rIdx][rIdx];
             }
         }
 
-    return lMatrix;
+    matrix.U = matrix.L;
+    mcmc::utils::TransposeSqMat(matrix.M);
 }
+} // namespace mcmc::utils
+#endif
