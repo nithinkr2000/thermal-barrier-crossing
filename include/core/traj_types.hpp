bbf792f399927a478157bdb49fe154f8e7186c75
nkr <nithinkr2000@gmail.com>
2026-04-19 21:52:21 +0200

    Added namespace for trajectory types.

--- include/core/traj_types.hpp
+++ include/core/traj_types.hpp
@@ -1,14 +1,16 @@
-#ifndef MCMC_TRAJ_TYPES_H
-#define MCMC_TRAJ_TYPES_H
+#ifndef MCMC_TRAJ_TYPES_HPP
+#define MCMC_TRAJ_TYPES_HPP
 
 #include "base_types.hpp"
 
 namespace mcmc::traj_types {
 
 struct Trajectory {
-    std::vector<Config> positions;
-    std::vector<Energy> energies;
+    std::vector<mcmc::base_types::Config> positions;
+    std::vector<mcmc::base_types::Energy> energies;
 };
 
+// TODO: Add type for tempering simulations that hold parameters, indices
+
 } // namespace mcmc::traj_types
-#endif // !MCMC_TRAJ_TYPES_H
+#endif
