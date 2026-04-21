23a0918f774f67c4f33dd9ed27a0179d63eb6725
nkr <nithinkr2000@gmail.com>
2026-04-21 02:11:14 +0200

    Added struct for gaussian kernel container.

--- include/kernels/gaussian_kernel.hpp
+++ include/kernels/gaussian_kernel.hpp
@@ -1,24 +1,38 @@
+#include "../core/base_types.hpp"
+#include "../utils/math_helpers.hpp"
 #include "kernel_params.hpp"
 #include <cassert>
 
 struct GaussianKernel : KernelParams {
     double amplitude;
-    Config avg;
-    Matrix cov;
+    mcmc::base_types::Config avg;
+    mcmc::base_types::Matrix<double> cov;
 
-    double evaluate(const Config &x) {
+    double evaluate(const mcmc::base_types::Config &x) {
+
         double e{amplitude};
 
-        size_t dim{avg.size()};
+        size_t dim{avg.v.size()};
+
+        auto &M = cov.getM();
+        auto &L = cov.getL();
+        auto &U = cov.getU();
+        auto &v = x.v;
 
         // Assert that the covariance matrix and mean have the same size.
-        assert(dim == cov.size());
-        // Assert that the input vector and mean have the same size.
-        assert(dim == x.size());
+        assert(dim == M.size());
+        assert(dim == L.size());
 
-        for (size_t idx{}; idx <= dim; ++idx) {
-            // TODO: invert covariance matrix
-        }
+        // Assert that the input vector and mean have the same size.
+        assert(dim == v.size());
+
+        mcmc::base_types::Config res(std::vector<double>(dim, 0.0f));
+
+        for (size_t idx{0}; idx < dim; ++idx)
+            res.v[idx] = mcmc::math_utils::dot(L[idx], v);
+
+        double exponent = -exponent * mcmc::math_utils::norm(res.v) / 2;
+        e *= std::exp(exponent);
 
         return e;
     }
